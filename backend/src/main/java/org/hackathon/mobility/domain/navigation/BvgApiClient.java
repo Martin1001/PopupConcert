a84f362dc75eead98befc368c06fa2cb171daf59
Thomas Wendzinski <thomas.wendzinski@asideas.de>
2016-12-04 00:59:37 +0100

    Adjustments

--- backend/src/main/java/org/hackathon/mobility/domain/navigation/BvgApiClient.java
+++ backend/src/main/java/org/hackathon/mobility/domain/navigation/BvgApiClient.java
@@ -1,6 +1,8 @@
 package org.hackathon.mobility.domain.navigation;
 
+import java.text.NumberFormat;
 import java.util.ArrayList;
+import java.util.Locale;
 
 import org.hackathon.mobility.domain.gigs.model.RoutingStep;
 import org.springframework.http.HttpMethod;
@@ -22,16 +24,24 @@ public class BvgApiClient {
             , HttpMethod.GET, clientHttpRequest -> {
             }, clientHttpResponse -> {
                 final ArrayList<RoutingStep> resRoutingSteps = new ArrayList<>();
-                final GigRoute res = new GigRoute(null, resRoutingSteps);
 
                 final JsonNode json = mapper.readTree(clientHttpResponse.getBody());
                 final JsonNode routingSteps = json.get("Trip").get(0).get("LegList").get("Leg");
+                String prize = null;
+                try{
+                    final JsonNode ticket = json.get("Trip").get(0).get("TariffResult").get("fareSetItem").get(0).get("fareItem").get(0).get("ticket").get(0);
+                    int prz = ticket.get("price").asInt(0);
+                    prize = NumberFormat.getCurrencyInstance(Locale.GERMAN).format(prz / 100d);
+
+                } catch (Exception e) {}
+
+                final GigRoute res = new GigRoute("" + prize, resRoutingSteps);
                 routingSteps.elements().forEachRemaining( step -> {
                     final JsonNode destStep = step.get("Destination");
                     final JsonNode product = step.get("Product");
                     RoutingStep.Line line = null;
                     if (product != null) {
-                        line = new RoutingStep.Line(product.get("name").asText(), step.get("direction").asText(""));
+                        line = new RoutingStep.Line(product.get("name").asText("").trim(), step.get("direction").asText("").trim());
                     }
                     resRoutingSteps.add(new RoutingStep(
                         destStep.get("name").asText(""),
--- backend/src/test/java/org/hackathon/mobility/domain/navigation/BvgApiClientTest.java
+++ backend/src/test/java/org/hackathon/mobility/domain/navigation/BvgApiClientTest.java
@@ -3,6 +3,8 @@ package org.hackathon.mobility.domain.navigation;
 
 import org.junit.Test;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 
 public class BvgApiClientTest {
 
@@ -12,6 +14,7 @@ public class BvgApiClientTest {
     public void fetchRoute() {
         final GigRoute res = client.fetchRoute("009013102", 52.500868D, 13.436392);
 
+        assertThat(res.getTotalPrice()).isNotNull();
         System.out.println("Res is: " + res);
 
     }
