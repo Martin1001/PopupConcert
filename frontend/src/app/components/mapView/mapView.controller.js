850a7993cd78be5d6cc07e169d1925fafabe53e4
Liang Shi <liang.shi@asideas.de>
2016-12-03 19:10:07 +0100

    added map

--- frontend/src/app/components/mapView/mapView.controller.js
+++ frontend/src/app/components/mapView/mapView.controller.js
@@ -1,23 +1,42 @@
 export default class MapController {
 
-    constructor($log, $scope, $timeout, NgMap) {
+    constructor($log, $http, $timeout, NgMap) {
         'ngInject';
-        this.googleMapsUrl="https://maps.googleapis.com/maps/api/js?key=AIzaSyBQpWnOkp6VOAUs_1AZlTZSCMe1R8GDhg0";
+        this.googleMapsUrl = "https://maps.googleapis.com/maps/api/js?key=AIzaSyBQpWnOkp6VOAUs_1AZlTZSCMe1R8GDhg0";
         this.$log = $log;
+        this.$http = $http;
         this.$timeout = $timeout;
+
+        this.playlist = [];
+        this.bandlist = [];
+
+        this.$http.get('/api/v1/gigs')
+            .then(res => {
+                this.playlist = res.data;
+                this.bandlist = res.data;
+            });
 
         NgMap.getMap().then((map) => {
             this.map = map;
-            this.marker = map.markers[0];
         });
+
     }
 
     centerChanged = (event) => {
         this.$log.info('center changed');
     };
+
     click = (event) => {
         this.$log.info('click');
-        this.map.setCenter(this.marker.getPosition());
-    }
+
+        let lat = event.latLng.lat().toPrecision(8);
+        let lon = event.latLng.lng().toPrecision(8);
+
+        this.bandlist = this.playlist.filter(b => b.location.lat == lat && b.location.lon == lon);
+    };
+
+    getTime = (millis) => {
+        return new Date(millis);
+    };
 }
 
