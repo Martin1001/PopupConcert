aa120132ddc335fe4c316917f802226cdb544d6f
Liang Shi <liang.shi@asideas.de>
2016-12-03 12:31:37 +0100

    everything works now, happay hacking

---
+++ backend/src/main/java/org/hackathon/mobility/Application.java
@@ -0,0 +1,11 @@
+package org.hackathon.mobility;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
